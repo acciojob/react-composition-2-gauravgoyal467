93b496f7922081965cc4920d44a9be1841306c3d
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-18 08:12:29 +0000

    3rd try

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Model = ({visible,onClose,children}) => {
     if (!visible) return null;
   return (
-    <div className='model-overlay'>
-        <button className="model-close" onClick={onClose}>Close</button>
+    <div className='model-overlay'onClick={onClose}>
+        <button className="model-close" >Close</button>
         {children}
     </div>
   )
