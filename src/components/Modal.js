f36fb3ca932883b5e56a8be824a9a244bf6c15ca
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-18 09:36:54 +0000

    8th try

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,8 +4,10 @@ const Model = ({visible,onClose,children}) => {
     if (!visible) return null;
   return (
     <div className='model-overlay'onClick={onClose}>
-        <button className="model-close" >Close</button>
-        <p className="model-p">{children}</p>
+        <div className="model-content">
+          <button className="model-close">Close</button>
+          <p className="model-p">{children}</p>
+        </div>
     </div>
   )
 }
